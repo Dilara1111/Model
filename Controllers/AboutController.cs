512f09f2a575f53165e7e234143efd5ed0a5d5b8
HuseynovaDilara <huseynova.8885@gmail.com>
2023-12-24 21:43:59 +0400

    Loop added

--- Controllers/AboutController.cs
+++ Controllers/AboutController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PurpleBuzz.Models;
+using PurpleBuzz.ViewModels.About;
 using System;
 
 namespace PurpleBuzz.Controllers
@@ -10,9 +11,13 @@ namespace PurpleBuzz.Controllers
         {
             List<Cards> cards = new List<Cards>
             {
-                new Cards {Id=1,FilePath = "display-4 bx bxs-bulb text-light",Title="Our Vision", Description = "Incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse commodo viverra.\r\n"}
+                new Cards {Id=1,FilePath = "display-4 bx bxs-bulb text-light",Title="Our Vision", Description = "Incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse commodo viverra.\r\n"},
+                new Cards {Id=2,FilePath = "display-4 bx bx-revision text-light",Title="Our Vision", Description = "Incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse commodo viverra.\r\n"},
+                new Cards {Id=3,FilePath = "display-4 bx bxs-select-multiple text-light",Title="Our Vision", Description = "Incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse commodo viverra.\r\n"}
             };
-            return View();
+            AboutIndexViewModel model = new AboutIndexViewModel { Cards = cards };
+                
+            return View(model);
         }
     }
 }
